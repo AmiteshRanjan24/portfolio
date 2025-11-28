bb2e93827b0dac40c6af88c2c9d263a826faacdf
Amitesh Ranjan <amiteshranjan5524@gmail.com>
2025-11-28 14:37:23 +0530

    feat(data): add socials data

---
+++ my-portfolio/src/data/socials1.js
@@ -0,0 +1,33 @@
+import { Linkedin, Github, Code2 } from "lucide-react";
+
+export const socialLinks1 = [
+  {
+    name: "LinkedIn",
+    handle: "@Amitesh Ranjan",
+    description:
+      "Let’s connect and collaborate on tech, projects, and career growth.",
+    url: "https://www.linkedin.com/in/amitesh-ranjan-089806279/",
+    icon: Linkedin,
+    color: "bg-blue-600",
+    logo: "/assets/linkedInImg.png",
+  },
+  {
+    name: "LeetCode",
+    handle: "@AmiteshRanjan",
+    description:
+      "Improving every day -500+ problems solved with a peak 1941 rating.",
+    url: "https://leetcode.com/u/AmiteshRanjan/",
+    icon: Code2,
+    color: "bg-amber-500",
+    logo: "/assets/leetcodeImg.png",
+  },
+  {
+    name: "GitHub",
+    handle: "@AmiteshRanjan24",
+    description: "Check out my work - open to collaboration and contributions.",
+    url: "https://github.com/AmiteshRanjan24",
+    icon: Github,
+    color: "bg-zinc-900",
+    logo: "/assets/gitHubImg.jpg",
+  },
+];
---
+++ my-portfolio/src/data/socials2.js
@@ -0,0 +1,32 @@
+import { Instagram, Code2, Trophy } from "lucide-react";
+export const socialLinks2 = [
+  {
+    name: "GeeksforGeeks",
+    handle: "@_amiteshranjan24",
+    description: "Solving daily challenges and diving into GFG tech. blogs.",
+    url: "https://www.geeksforgeeks.org/profile/_amiteshranjan24",
+    icon: Trophy,
+    color: "bg-indigo-600",
+    logo: "/assets/gfgImg.png",
+  },
+  {
+    name: "Instagram",
+    handle: "@_amitesh_ranjan_",
+    description:
+      "A glimpse into my life - a little more of who I am and what drives me.",
+    url: "https://instagram.com/_amitesh_ranjan_",
+    icon: Instagram,
+    color: "bg-pink-600",
+    logo: "/assets/instagramImg.jpg",
+  },
+  {
+    name: "CodeChef",
+    handle: "@amitesh_ranjan",
+    description:
+      "Improving my contest strategy and problem-solving speed — 2★ coder",
+    url: "https://www.codechef.com/users/amitesh_ranjan",
+    icon: Trophy,
+    color: "bg-indigo-600",
+    logo: "/assets/codechefImg.jpg",
+  },
+];
