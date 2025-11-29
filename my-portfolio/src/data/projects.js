c4fa81c4a7d875c7b3cafd7b47a080122e2b6c3f
Amitesh Ranjan <amiteshranjan5524@gmail.com>
2025-11-29 17:11:13 +0530

    feat(data): add project data

---
+++ my-portfolio/src/data/projects.js
@@ -0,0 +1,23 @@
+export const projects = [
+  {
+    title: "FlexiRent Backend",
+    desc: "Backend service for FlexiRent, a rental platform that enables users to rent vehicles from different providers. It provides role-based access for Users and Providers with secure authentication, authorization, and data management.",
+    tech: ["Node.js", "Express.js", "MongoDB", "JWT", "Zod", "Bcrypt"],
+    github: "https://github.com/AmiteshRanjan24/FlexiRent-backend",
+    demo: "",
+  },
+  {
+    title: "Bookstore Application",
+    desc: "A comprehensive RESTful API for managing a bookstore application with user authentication, book management, and advanced filtering capabilities.",
+    tech: ["Node.js", "Express.js", "MongoDB", "JWT", "Zod", "Bcrypt"],
+    github: "https://github.com/AmiteshRanjan24/Bookstore_Application",
+    demo: "",
+  },
+  {
+    title: "Car Rental System",
+    desc: "A robust, console-based Car Rental System built in C++ that demonstrates core Object-Oriented Programming (OOP) principles. This application simulates a real-world car rental service, allowing users to browse available vehicles, rent cars for specific durations, and generate automated invoices.",
+    tech: ["C++", "Object Oriented Programming"],
+    github: "https://github.com/AmiteshRanjan24/Car_Rental_System",
+    demo: "",
+  },
+];
